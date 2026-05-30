63ccbbfd905d7427720f9f2df0964b16c0903b51
Shane McIntosh <shmcinto@gmail.com>
2026-05-30 17:37:17 -0400

    Add project files.

---
+++ Alarm-Monitoring-Console/DC09Parser.cs
@@ -0,0 +1,245 @@
+﻿using Nito.HashAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Numerics;
+using System.Text;
+
+namespace Alarm_Monitoring_Console
+{
+    internal class DC09Parser
+    {
+        private string _rawMessage = string.Empty;
+        private string _messageCRC = string.Empty;
+        private string _messageLength = string.Empty;
+        private string _protocolID = string.Empty;
+        private string _messageSeq = string.Empty;
+        private string _receiverNum = string.Empty;
+        private string _linePrefix = string.Empty;
+        private string _accountNum = string.Empty;
+        private string _messageData = string.Empty;
+        private string _messageXData = string.Empty;
+        private string _messageTimestamp = string.Empty;
+
+        public string RawMessage { get { return _rawMessage; } }
+        public string MessageCRC { get {  return _messageCRC; }  }
+        public string MessageLength { get { return _messageLength; } }
+        public string ProtocolID { get { return _protocolID.Replace("\"",""); } }
+        public string MessageSeq { get { return _messageSeq; }  }
+        public string ReceiverNum { get { return _receiverNum.Replace("R", ""); ; }  }
+        public string LinePrefix { get { return _linePrefix.Replace("L", "") ; }  }
+        public string AccountNumber {  get {  return _accountNum.Replace("#",""); }  }
+        public string MessageData {  get {  return _messageData; } } 
+        public string MessageXData {  get { return _messageXData; }  }
+        public string MessageTimestamp {  get { return _messageTimestamp.Replace("_",""); } }
+
+        public DC09Parser(string rawMessage)
+        {
+            ParseMessage(rawMessage);
+        }
+
+        public DC09Parser()
+        {
+        }
+
+        public bool ParseMessage(string rawMessage)
+        {
+            _rawMessage = rawMessage;
+
+            //Confirm message starts with LF and ends with CR if not we know right away that it's not a valid Ademco CID message
+            if ((rawMessage[0] != '\n') || (rawMessage[^1] != '\r'))
+            {
+                return false;
+            }
+
+            int curPos = 1;
+            int rLen = 0;
+
+            _messageCRC = rawMessage.Substring(curPos, 4); //CRC comes right after the LF character and is 4 characters long
+            curPos += 4;
+
+            //TODO Add CRC Check Here
+
+            _messageLength = rawMessage.Substring(curPos, 4); //Message length starts with 0 and is 3 hex digits right after the CRC
+            curPos += 4;
+
+            //TODO Add Message Length Check Here
+
+            rLen = rawMessage.IndexOf('"', curPos + 1);
+            rLen = rLen - curPos+1;
+            _protocolID = rawMessage.Substring(curPos, rLen); //Protocol ID is 8 characters including the "" characters surrounding it
+            curPos += rLen;
+
+            _messageSeq = rawMessage.Substring(curPos, 4);
+            curPos += 4;
+
+            if (rawMessage[curPos] == 'R') {
+
+                //Can be 1-6 digits after R so we need to find the next field which starts with L
+                rLen = rawMessage.IndexOf('L', curPos);
+                rLen = rLen - curPos;
+                _receiverNum = rawMessage.Substring(curPos, rLen);
+                curPos += rLen;
+            }
+
+            rLen = rawMessage.IndexOf('#');
+            if ((rLen <= 0) || (rLen <= curPos))
+                return false; // no # present message is invalid
+
+            rLen = rLen - curPos;
+
+            _linePrefix = rawMessage.Substring(curPos, rLen);
+            curPos += rLen;
+
+            //After Line Prefix comes Account number and is prefixed with # and can be 3-16 characters expects to see [ as the next character
+            rLen = rawMessage.IndexOf('[');
+            if ((rLen <= 0) || (rLen <= curPos))
+                return false; // no [ present message is invalid
+            rLen = rLen - curPos;
+            _accountNum = rawMessage.Substring(curPos, rLen);
+            curPos += rLen;
+
+            //Next we pull the main message body, surrounded by []
+            rLen = rawMessage.IndexOf(']');
+            if ((rLen <= 0) || (rLen <= curPos))
+                return false; // no ] present message is invalid
+            rLen = rLen - curPos + 1;
+            _messageData = rawMessage.Substring(curPos, rLen);
+            curPos += rLen;
+
+            //Next we have to check to see if we have any Extended Data
+            if (rawMessage[curPos] == '[')
+            {
+                rLen = rawMessage.IndexOf(']',curPos);
+                if ((rLen <= 0) || (rLen <= curPos))
+                    return false; // no ] present message is invalid
+                rLen = (rLen - curPos) + 1;
+                _messageXData = rawMessage.Substring(curPos, rLen);
+                curPos += rLen;
+            }
+
+            //Next we check to see if we have a timestamp, if so it's 20 characters and we pull it otherwise we're done
+            if (rawMessage[curPos] == '_') {
+                _messageTimestamp = rawMessage.Substring(curPos, 20);//Timestamp is 20 characters
+            }
+
+            return true;
+        }
+
+        public Dictionary<string, string> GetDictionary()
+        {
+
+            Dictionary<string, string> kvPairs = new Dictionary<string, string>();
+
+            kvPairs.Add("RawMessage", _rawMessage.Replace("\n", "\\n").Replace("\r", "\\r"));
+            kvPairs.Add("MessageCRC", MessageCRC);
+            kvPairs.Add("MessageLength", MessageLength);
+            kvPairs.Add("ProtocolID", ProtocolID);
+            kvPairs.Add("MessageSeq", MessageSeq);
+            kvPairs.Add("RecieverNum", ReceiverNum);
+            kvPairs.Add("LinePrefix", LinePrefix);       
+            kvPairs.Add("AccountNumber", AccountNumber);
+            kvPairs.Add("MessageData", MessageData);
+            kvPairs.Add("MessageXData", MessageXData);
+            kvPairs.Add("TimeStamp", MessageTimestamp);
+
+            return kvPairs;
+           
+        }
+
+        public bool CheckCRCMatch()
+        {
+            string currentCRC = CreateInternalCRC();
+            //Console.WriteLine("Current CRC: " + currentCRC);
+            //Console.WriteLine("Stored  CRC: " +  _messageCRC);
+            if (currentCRC == _messageCRC)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private string CreateInternalCRC()
+        {
+
+            string crcString = (_protocolID + _messageSeq + _receiverNum + _linePrefix + _accountNum + _messageData + _messageXData + _messageTimestamp);
+            return DC09Parser.CreateCRC(crcString);
+
+        }
+
+        public static string CreateCRC(string rawData)
+        {
+            int startPos = rawData.IndexOf('"');
+            int endPos = rawData.Length;
+            if (rawData[endPos - 1] == '\r')
+                endPos--;
+
+            string crcString = rawData.Substring(startPos,endPos-startPos);
+            byte[] crcBytes = Encoding.ASCII.GetBytes(crcString);
+            byte[] crcCalc = new byte[2];
+            byte tmp;
+
+            var calc = new CRC16();
+            crcCalc = calc.ComputeHash(crcBytes);
+
+            //Flip the bits around to get the correct CRC
+            tmp = crcCalc[0];
+            crcCalc[0] = crcCalc[1];
+            crcCalc[1] = tmp;
+
+            string? crc = Convert.ToHexString(crcCalc);
+            if (crc != null)
+            {
+                return crc;
+            }
+            return string.Empty;
+        }
+
+        //Creates an ACK String
+        //ACK Format: <LF><CRC><0LLL><"ACK"><seq><Rrcvr><Lpref><#acct>[]<CR>
+
+        /// <summary>
+        /// Creates an ACK String message in response to a NULL message for Monitoring
+        /// </summary>
+        /// <param name="seq">MUST BE 4 DIGITS</param>
+        /// <param name="rcvr">1-6 Digits in length if present</param>
+        /// <param name="linePrefix">1-6 Digits in length</param>
+        /// <param name="acctNum">3-16 Digits in length</param>
+        /// <returns>string ready to be sent over the network</returns>
+        public static string CreateACK(string seq, string rcvr, string linePrefix, string acctNum)
+        {
+            string rawData = string.Empty;
+            if (rcvr != string.Empty)
+                rawData = $"\"ACK\"{seq}R{rcvr}L{linePrefix}#{acctNum}[]\r";
+            else
+                rawData = $"\"ACK\"{seq}L{linePrefix}#{acctNum}[]\r";
+            rawData = "\n" + DC09Parser.CreateCRC(rawData) + DC09Parser.CalculateLength(rawData) + rawData;
+
+            return rawData;
+        }
+
+        public static string CalculateLength(string rawData)
+        {
+            int startPos = rawData.IndexOf('"');
+            int endPos = rawData.Length - 1;
+
+            int messageLen = endPos - startPos;
+            return "0" + messageLen.ToString("X3");
+        }
+
+        public static string GenerateTimeStamp()
+        {
+            //Get GMT Time and then create it in a specific format, then add _ to the beginning of it.
+            //<_HH:MM:SS,MM-DD-YYYY> <-Date Time Format that needs to be returned
+            DateTime gmtNow = DateTime.UtcNow;
+
+            string formattedDate = gmtNow.ToString("HH:mm:ss,MM-dd-yyyy", CultureInfo.InvariantCulture);
+            formattedDate = "_" + formattedDate;
+            return formattedDate;
+        }
+
+
+    }
+}
+ 
---
+++ Alarm-Monitoring-Console/Program.cs
@@ -0,0 +1,257 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Alarm_Monitoring_Console
+{
+    internal class Program
+    {
+
+        private const int _listenPort = 3060;
+        private static Socket _listenSocket;
+        private static List<Socket> _clientSockets = new List<Socket>();
+
+        static void Main(string[] args)
+        {
+
+            bool isRunning = true;
+            _listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _listenSocket.Bind(new IPEndPoint(IPAddress.Any, _listenPort));
+            _listenSocket.Listen(10);
+
+            ShowBanner();
+            Console.WriteLine($"Server started on port {_listenPort}. Polling for activity...");
+
+            do
+            {
+
+                //Repopulate list
+                List<Socket> readList = new List<Socket> { _listenSocket };
+                readList.AddRange(_clientSockets);
+
+                //Poll the sockets for data
+                Socket.Select(readList, null, null, 100000);
+
+                //Loop through the ready sockes and handle them accordingly
+                foreach (Socket sock in readList)
+                {
+                    if (sock == _listenSocket) //There's a new Connection!
+                        HandleNewConnection();
+                    HandleClientData(sock);
+                }
+
+                //Remove closed/invalid sockets from our tracking
+                _clientSockets.RemoveAll(x => !x.Connected);
+
+                //Main loop for the console application
+                if (CheckForQuitKey())
+                {
+                    isRunning = false;
+                }
+
+            } while (isRunning);
+        }
+
+        private static void HandleNewConnection() {
+        
+            try
+            {
+                Socket newClient = _listenSocket.Accept();
+                _clientSockets.Add(newClient);
+                LogMessage($"[CONNECTED] - Client Connected from {newClient.RemoteEndPoint}");
+            } catch(SocketException ex)
+            {
+                LogMessage($"Accept Error: {ex.Message}");
+            }
+
+        }
+
+        private static void HandleClientData(Socket client) {
+
+            //We want to read up to the first \r since our messages are wrapped in 
+            //\n{data}/r
+            //Once we have it we process it
+            MemoryStream memStream = new MemoryStream();
+            string rawMessage = string.Empty;
+            byte[] singleBuff = new byte[1];
+            try
+            {
+                while (true)
+                {
+                    int bytesRead = client.Receive(singleBuff);
+                    if (bytesRead == 0)
+                    {
+                        //throw new SocketException((int)SocketError.ConnectionReset);
+                        LogMessage("Disconnect");
+                    }
+
+                    memStream.WriteByte(singleBuff[0]);
+
+                    if (singleBuff[0] == '\r')//Have we reached our terminating charactter
+                        break;
+                }
+
+                rawMessage = Encoding.ASCII.GetString(memStream.ToArray());
+
+                //This is where we handle our actual recieved message:
+                LogMessage($"[RECIEVED] - FROM {client.RemoteEndPoint} : {rawMessage}");
+                HandleProtocolData(rawMessage);
+
+            }
+            catch (SocketException ex)
+            {
+                //LogMessage($"[DISCONNECTED] - Client forceful Disconnect");
+                //CloseClient(client);
+            }
+
+
+        }
+
+        private static void HandleProtocolData(string rawData)
+        {
+            DC09Parser parser = new DC09Parser(rawData);
+            Dictionary<string, string> parsedData = parser.GetDictionary();
+            foreach (KeyValuePair<string, string> pair in parsedData)
+            {
+
+                LogMessage($"{pair.Key} : {pair.Value}");
+            
+            }
+            LogMessage($"Checksum Validation: {parser.CheckCRCMatch()}");
+        }
+
+        private static void CloseClient(Socket client)
+        {
+            try
+            {
+                client.Shutdown(SocketShutdown.Both);
+            }
+            catch { /* Ignore if already down */ }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+
+        private static void ShowBanner()
+        {
+            Console.WriteLine("#####################################################");
+            Console.WriteLine("#                                                   #");
+            Console.WriteLine("#               Alarm Monitoring Console            #");
+            Console.WriteLine("#              Created by Shane McIntosh            #");
+            Console.WriteLine("#           Copyright (c) 2026 RexFault-Labs        #");
+            Console.WriteLine("#                                                   #");
+            Console.WriteLine("#####################################################");
+            Console.WriteLine();
+            Console.WriteLine("Press the Q key at any time to quit the application.");
+        }
+
+        /// <summary>
+        /// Checks to see if the Q key has been pressed to quit the application. 
+        /// This allows the user to exit the console gracefully without having to force close it.
+        /// </summary>
+        /// <returns>
+        /// Returns true if the Q key has been pressed, otherwise returns false.
+        /// </returns>
+        private static bool CheckForQuitKey()
+        {
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Q)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void DC09ParserTest()
+        {
+
+            //Test Cases
+            string rawTestMessage1 = "\n9EC40027\"ADM-CID\"0001L0#1002[#1002|1602 00 001]\r";
+            string rawTestMessage2 = "\n75140027\"ADM-CID\"0001L0#1002[#1002|3354 00 004]\r";
+            //Old Tests
+            //string rawTestMessage3 = "\nBE780027\"ADM-CID\"0041L0#1001[#1001|1602 00 030]\r";
+            //string rawTestMessage4 = "\n4B540027\"ADM-CID\"0037L0#1001[#1001|3354 00 004]\r";
+            //New Tests
+            //Common Values in the messsage below are:
+            //SEQ: 9876   RCVR: 579BDF pref: 789ABC acct: 12345A
+            //Fire Alarm, Zone 129, SIA DC-04 Format, Non-Encrypted, No Timestamp, xData: MAC Address x1234567890AB
+            //string rawTestMessage3 = "\n8C860043\"SIA-DCS\"9876R579BDFL789ABC#123456A[#123456A|NFA129][M1234567890AB]\r";
+            //Fire Alarm, Zone 129, SIA DC-04 Format, Non-Encrypted, With TimeStamp, xData: MAC Address x1234567890AB
+            //CRC & LENGTH will not CHECK OUT
+            string rawTestMessage3 = "\n8C860043\"SIA-DCS\"9876R579BDFL789ABC#123456A[#123456A|NFA129][M1234567890AB]_13:14:15,02-15-2006\r";
+            //Fire Alarm, Zone 129, SIA DC-04 Format, Non-Encrypted, With Timestamp
+            string rawTestMessage4 = "\nDC530046\"SIA-DCS\"9876R579BDFL789ABC#12345A[#12345A|NFA129]_13:14:15,02-15-2006\r";
+            //Unencrypted NULL Message for HeartBeat
+            //<LF><CRC><0LLL><"NULL"><0000><Rrcvr><Lpref><#acct>[]<timestamp><CR>
+            string rawTestMessage5 = "\"NULL\"0001R01L01#3501[]_13:14:15,02-15-2006\r";
+            Console.WriteLine("CRC1: " + DC09Parser.CreateCRC(rawTestMessage5));
+            rawTestMessage5 = "\n" + DC09Parser.CreateCRC(rawTestMessage5) + DC09Parser.CalculateLength(rawTestMessage5) + rawTestMessage5;
+            Console.WriteLine("CRC2: " + DC09Parser.CreateCRC(rawTestMessage5));
+            //rawTestMessage5 = "\n" + rawTestMessage5;
+
+            //SIA DC-09 Parser Testing
+            DC09Parser p1 = new DC09Parser(rawTestMessage1);
+            DC09Parser p2 = new DC09Parser(rawTestMessage2);
+            DC09Parser p3 = new DC09Parser(rawTestMessage3);
+            DC09Parser p4 = new DC09Parser(rawTestMessage4);
+            DC09Parser p5 = new DC09Parser(rawTestMessage5);
+            
+            Console.WriteLine("TestMessage1: ");
+            foreach (KeyValuePair<string, string> pair in p1.GetDictionary())
+            {
+                Console.WriteLine($"\t{pair.Key} : {pair.Value}");
+            }
+            //Console.WriteLine("CRC Check: " + p1.CreateInternalCRC());
+            Console.WriteLine("\tCRC Test: " + p1.CheckCRCMatch());
+            Console.WriteLine("\tMSG Length: " + DC09Parser.CalculateLength(rawTestMessage1));
+
+            Console.WriteLine("TestMessage2: ");
+            foreach (KeyValuePair<string, string> pair in p2.GetDictionary())
+            {
+                Console.WriteLine($"\t{pair.Key} : {pair.Value}");
+            }
+            //Console.WriteLine("CRC Check: " + p2.CreateInternalCRC());
+            Console.WriteLine("\tCRC Test: " + p2.CheckCRCMatch());
+            Console.WriteLine("\tMSG Length: " + DC09Parser.CalculateLength(rawTestMessage2));
+
+            Console.WriteLine("TestMessage3: ");
+            foreach (KeyValuePair<string, string> pair in p3.GetDictionary())
+            {
+                Console.WriteLine($"\t{pair.Key} : {pair.Value}");
+            }
+            //Console.WriteLine("CRC Check: " + p3.CreateInternalCRC());
+            Console.WriteLine("\tCRC Test: " + p3.CheckCRCMatch());
+            Console.WriteLine("\tMSG Length: " + DC09Parser.CalculateLength(rawTestMessage3));
+
+            Console.WriteLine("TestMessage4: ");
+            foreach (KeyValuePair<string, string> pair in p4.GetDictionary())
+            {
+                Console.WriteLine($"\t{pair.Key} : {pair.Value}");
+            }
+            //Console.WriteLine("CRC Check: " + p4.CreateInternalCRC());
+            Console.WriteLine("\tCRC Test: " + p4.CheckCRCMatch());
+            Console.WriteLine("\tMSG Length: " + DC09Parser.CalculateLength(rawTestMessage4));
+
+            Console.WriteLine("TestMessage5: ");
+            foreach (KeyValuePair<string, string> pair in p5.GetDictionary())
+            {
+                Console.WriteLine($"\t{pair.Key} : {pair.Value}");
+            }
+            //Console.WriteLine("CRC Check: " + p4.CreateInternalCRC());
+            Console.WriteLine("\tCRC Test: " + p5.CheckCRCMatch());
+            Console.WriteLine("\tMSG Length: " + DC09Parser.CalculateLength(rawTestMessage5));
+        }
+
+        private static void LogMessage(string message)
+        {
+
+            Console.WriteLine(message); 
+        }
+
+    }
+}
